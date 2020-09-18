a96c8360a1a693c82c9beed5086a83dca1e21cbb
99002573 <71497022+99002573@users.noreply.github.com>
2020-09-18 18:21:01 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
-
 #include "Calculator.h"
 
-
-
 int main()
-
 {
-
   int choice = 0;
-
   do{
-
     printf("1. Addition\n");
 
     printf("2. Subtraction\n");
@@ -31,31 +24,17 @@ int main()
       printf("\n### Invalid Entry | Enter Again ###\n\n");
 
       continue;
-
     }
-
     if(choice == 5){
-
       break;
-
     }
-
     int number1 = 0, number2 = 0;
-
     printf("Enter first number: ");
-
     scanf("%d", &number1);
-
     printf("Enter second number: ");
-
     scanf("%d", &number2);
-
-
-
     int result = calculator(choice, number1, number2);
-
     printf("Answer = %d", result);
-
   }while(choice!=5);
 
 }
