56a438a67a2d0b30f2dbb9bbe945528bf423a0da
99002573 <71497022+99002573@users.noreply.github.com>
2020-09-18 20:49:54 +0530

    Update Calculator.h

--- inc/Calculator.h
+++ inc/Calculator.h
@@ -14,7 +14,21 @@ int multiplication(int number1, int number2);
 
 int division(int number1, int number2);
 
+int Positive_or_Negative(int number1);
+
+int Max_of_two(int number1, int number2);
+
+int Factorial(int number1);
+
+int Prime(int number1);
+
+int Average(int number1, int number2);
+
+int Swap(int number1, int number2);
+
 int calculator(int choice, int number1, int number2);
+
+
 
 
 
