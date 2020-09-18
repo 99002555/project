18ef97a408ed530e9b8525af592045c93dc27950
99002573 <71497022+99002573@users.noreply.github.com>
2020-09-18 21:44:34 +0530

    Update Calculator.c

--- src/Calculator.c
+++ src/Calculator.c
@@ -1,31 +1,24 @@
-#include <stdio.h>
-int addition(int a, int b){
-  return (a + b);
-}
+#include <calculator_operations.h>
 
-int subtraction(int a, int b){
-  return (a - b);
-}
-
-int multiplication(int a, int b){
-  return (a * b);
-}
-
-int division(int a, int b){
-  if(b == 0){
-    printf("Denominator cannot be 0");
-    return -1;
-  }
-  return (a / b);
-}
-
-int calculator(int choice, int x, int y)
+int add(int operand1, int operand2)
 {
-  int (*calcPtr[4])(int, int);
-  calcPtr[0] = addition;
-  calcPtr[1] = subtraction;
-  calcPtr[2] = multiplication;
-  calcPtr[3] = division;
-  int result = (*calcPtr[choice-1]) (x, y);
-  return result;
+    return operand1 + operand2;
+}
+
+int subtract(int operand1, int operand2)
+{
+    return operand1 - operand2;
+}
+
+int multiply(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int divide(int operand1, int operand2)
+{
+    if(0 == operand2)
+        return 0;
+    else
+        return operand1 / operand2;
 }
